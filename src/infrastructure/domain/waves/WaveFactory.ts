3c9d086ae397c3e19619e0c173fe5734f8336b5d
kou1046 <iwashiro0517@yahoo.co.jp>
2023-08-21 22:39:27 +0900

    propagateメソッドの追加

--- src/infrastructure/domain/waves/WaveFactory.ts
+++ src/infrastructure/domain/waves/WaveFactory.ts
@@ -1,5 +1,11 @@
 import nj from "numjs";
 
+import {
+  topShift,
+  leftShift,
+  rightShift,
+  bottomShift,
+} from "../../utils/shift";
 import { Grid } from "../grids/Grid";
 import { Wave } from "./Wave";
 import { Walls } from "../walls/Walls";
@@ -22,8 +28,26 @@ export class WaveFactory {
   }
 
   public create() {
-    return new Wave(this.waveArray);
+    const newWaveArray = this.propagate();
+    return new Wave(newWaveArray);
   }
 
-  private propagate() {}
+  private propagate(): nj.NdArray<number[]> {
+    const vT = topShift(this.waveArray);
+    const vB = bottomShift(this.waveArray);
+    const vR = rightShift(this.waveArray);
+    const vL = leftShift(this.waveArray);
+
+    const v1 = this.waveArray.multiply(2).subtract(this.preWaveArray);
+
+    const v2_1 = vL.add(vR).add(vB).add(vT);
+    const v2_2 = this.waveArray.multiply(4);
+    const v2_3 = v2_1.subtract(v2_2);
+
+    const v2 = v2_3.multiply(this.grid.alpha());
+
+    const newWaveArray = v1.add(v2);
+
+    return newWaveArray;
+  }
 }
