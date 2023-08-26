0abf76aba7e63b7816690b865d6f24e1fe7c5422
iwashiro <iwashiro0517@yahoo.co.jp>
2023-08-27 00:11:35 +0900

    描画確認用にindex.tsxを修正，ビルド

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,30 @@ import reactDom from "react-dom";
 import WaveMap from "./components/WaveMap";
 
 import { Grid } from "./infrastructure/domain/grids/Grid";
+import { DiricreWall } from "./infrastructure/domain/walls/DiricreWall";
+import { Walls } from "./infrastructure/domain/walls/Walls";
+import { diricreCornerFactory } from "./infrastructure/domain/corners/DiricreCornerFactory";
 import { Wave } from "./infrastructure/domain/waves/Wave";
 import { WaveFactory } from "./infrastructure/domain/waves/WaveFactory";
 
-const WIDTH = 2.0;
-const HEIGHT = 2.0;
+const WIDTH = 1.0;
+const HEIGHT = 1.0;
 const H = 0.04; // 空間刻み幅 (解像度)
 const DT = 0.01; //　更新時間幅
 const RAD = 10; // ガウス入力の半径 (入力の鋭さ)
 
+const topWall = new DiricreWall([0.5, 1.0], [1.0, 1.0], "bottom");
+const rightWall = new DiricreWall([1.0, 1.0], [1.0, 0.5], "left");
+const bottomWall = new DiricreWall([1.0, 0.5], [0.5, 0.5], "top");
+const leftWall = new DiricreWall([0.5, 0.5], [0.5, 1.0], "right");
+
+const walls = new Walls(
+  [topWall, rightWall, bottomWall, leftWall],
+  diricreCornerFactory
+);
+
 const grid = new Grid(WIDTH, HEIGHT, H, DT);
-const waveFactory = new WaveFactory(grid);
+const waveFactory = new WaveFactory(grid, walls);
 waveFactory.inputGauss(0, HEIGHT / 2, RAD);
 
 export default function Home() {
